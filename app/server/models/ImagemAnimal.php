2cd3ef0c52eb6b251ce6599c0b94caf0f656cb6c
learnjsfatec <learnjs_fatec@outlook.com>
2018-05-23 08:46:48 -0300

    Update ImagemAnimal.php

--- app/server/models/ImagemAnimal.php
+++ app/server/models/ImagemAnimal.php
@@ -19,14 +19,27 @@
             return false;
         }
 
-        public function all()
+        public function find($id)
         {
-            return Conn::getConn()->query("select * from Imagens where flag='animal'")->fetchAll(PDO::FETCH_ASSOC);
+            return Conn::getConn()->query("select * from imagens where flag='animal' and id_foreign=".$id)->fetchAll(PDO::FETCH_ASSOC);
         }
 
+		public function all()
+        {
+            return Conn::getConn()->query("select * from imagens where flag='animal'")->fetchAll(PDO::FETCH_ASSOC);
+        }
+		
         public function trash($id) 
         {
-           $imagem = Conn::getConn()->query("select nome_imagem from Imagens where id=".$id)->fetch(PDO::FETCH_ASSOC);
-           return (Conn::getConn()->query("delete from Imagens where id=".$id) == true) and (unlink("./app/client/assets/imagens/animais/".$imagem["nome_imagem"]));
+            $imagem = Conn::getConn()->query("select nome_imagem from imagens where id=".$id)->fetch(PDO::FETCH_ASSOC);
+            $st = Conn::getConn()->prepare("delete from imagens where id=?");
+            $st->bindParam(1, $id);
+            $st->execute();
+            if ($st->rowCount() > 0)
+            {
+                unlink("./app/client/assets/imagens/animais/".$imagem["nome_imagem"]);
+                return true;
+            }
+            return false;
         }
     }
